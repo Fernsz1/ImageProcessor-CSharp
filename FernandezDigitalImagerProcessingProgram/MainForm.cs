bd4bf8167a708bf86814326ad54b74a2558556ba
Luis <chrisluisfern1@gmail.com>
2025-09-30 19:40:34 +0800

    cleanup

--- FernandezDigitalImagerProcessingProgram/MainForm.cs
+++ FernandezDigitalImagerProcessingProgram/MainForm.cs
@@ -67,7 +67,7 @@ namespace FernandezDigitalImagerProcessingProgram
                 }
             }
         }
-
+        // HELOOOOOOOOOOOOOOOOOO
         // Choose Image
         private void button1_Click(object sender, EventArgs e)
         {
